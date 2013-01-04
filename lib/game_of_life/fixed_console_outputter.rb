d19561b06dcab44981a18961a38e64c64bda22b3
Sandro Padin <sandropadin@gmail.com>
2013-01-04 16:36:50 -0600

    Adding console outputter.

---
+++ lib/game_of_life/fixed_console_outputter.rb
@@ -0,0 +1,22 @@
+module GameOfLife
+  class FixedConsoleOutputter
+    attr_accessor :output
+
+    def initialize(output)
+      @output = output
+    end
+
+    def show_grid(grid)
+      10.times do |row|
+        10.times do |col|
+          if grid.has_living_cell_at?([row, col])
+            output.print "X"
+          else
+            output.print "-"
+          end
+        end
+        output.print "\n"
+      end
+    end
+  end
+end
---
+++ spec/game_of_life/fixed_console_outputter_spec.rb
@@ -0,0 +1,32 @@
+require 'game_of_life/fixed_console_outputter'
+require "game_of_life/grid"
+require "stringio"
+
+module GameOfLife
+  describe FixedConsoleOutputter do
+    it "outputs the grid" do
+      grid = Grid.new
+      grid.bring_cell_to_life_at([0,0])
+      grid.bring_cell_to_life_at([0,1])
+      grid.bring_cell_to_life_at([0,2])
+      grid.bring_cell_to_life_at([9,9])
+      output = StringIO.new
+      outputter = FixedConsoleOutputter.new(output)
+      outputter.show_grid(grid)
+
+      expected_output = <<END
+XXX-------
+----------
+----------
+----------
+----------
+----------
+----------
+----------
+----------
+---------X
+END
+      output.string.strip.should == expected_output.strip
+    end
+  end
+end
