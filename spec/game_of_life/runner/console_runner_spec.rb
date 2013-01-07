fbe2ee2ca4410335d637dd28bbce2d0fb416f305
Sandro Padin <sandropadin@gmail.com>
2013-01-07 10:43:34 -0600

    Cleaning up spec.

--- spec/game_of_life/runner/console_runner_spec.rb
+++ spec/game_of_life/runner/console_runner_spec.rb
@@ -4,12 +4,13 @@ module GameOfLife
   module Runner
     describe ConsoleRunner do
       let(:outputter) { mock("outputter", :show_grid => nil) }
+      let(:grid) { GameOfLife::GridLoader::JsonGrid.load("data/grid.json") }
 
       it "traps INT signal" do
         GameOfLife::Runner::ConsoleRunner.any_instance.should_receive(:trap).with(:INT)
-        grid = GameOfLife::GridLoader::JsonGrid.load("data/grid.json")
-        runner = GameOfLife::Runner::ConsoleRunner.new(grid, outputter)
-        blk = lambda { raise Interrupt }
+        GameOfLife::Runner::ConsoleRunner.new(grid, outputter)
+        
+        lambda { raise Interrupt }
       end
     end
   end
